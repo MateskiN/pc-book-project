e19bd9495388e9393a799afb92e1c9f54fc031c7
Nikola Mateski <nikola.mateski@outlook.com>
2021-05-28 21:33:35 +0200

    initial commit

---
+++ src/main/java/com/nikolamateski/pcbooklibrary/PcBookLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.nikolamateski.pcbooklibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcBookLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcBookLibraryApplication.class, args);
+	}
+
+}
